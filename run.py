29eef2abc447f5537600309da4008eae5725243e
goschtl <ck@novareto.de>
2017-09-29 10:54:52 +0200

    update

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import os
+
 from cookiecutter.main import cookiecutter
+
+OUTPUT = "/tmp/cc/"
 
 
 if __name__ == '__main__':
     project_path = os.path.abspath(os.path.dirname(__file__))
-    cookiecutter(project_path, extra_context={'project_path': project_path})
+    output_path = os.path.abspath(OUTPUT)
+    print "PROJECTPATH", project_path
+    cookiecutter(project_path, extra_context={'project_path': project_path}, output_dir=OUTPUT)
