ea84b213ff8f5a7cdc663e07dfbf5b35984d076d
Christian Klinger <ck@novareto.de>
2018-02-07 16:56:49 +0100

    check

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-openssl genrsa -out apache/privkey.pem 1024
-openssl rsa -in apache/privkey.pem -out apache/pubkey.pem -pubout
+#openssl genrsa -out apache/privkey.pem 1024
+#openssl rsa -in apache/privkey.pem -out apache/pubkey.pem -pubout
 #python3 bootstrap.py && bin/buildout
