bb5c813329579531193ee47337d2b25c771e0dbb
Christian Klinger <ck@novareto.de>
2018-02-07 16:39:56 +0100

    changes

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python3 bootstrap.py && bin/buildout
+openssl genrsa -out apache/privkey.pem 1024
+openssl rsa -in apache/privkey.pem -out apache/pubkey.pem -pubout
+#python3 bootstrap.py && bin/buildout
