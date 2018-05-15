5bbb51ececd1550e9c5cb8d490c11464d77c736c
Christian Klinger <ck@novareto.de>
2018-05-15 11:55:50 +0200

    update

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-virtualenv . -p "/usr/bin/python3.5"
-source bin/activate
-pip install zc.buildout==2.11.3
+python3.6 -m venv .
+./bin/pip install zc.buildout==2.11.3
 ./bin/buildout
-python generate_keys.py
-pip install mod_wsgi
+./bin/python generate_keys.py
+./bin/pip install mod_wsgi
