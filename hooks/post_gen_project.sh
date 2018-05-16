f1d33dcf119ab3525b70ecd60d5ebb623aef8d13
Christian Klinger <ck@novareto.de>
2018-05-16 16:29:54 +0200

    fixes

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 python3.6 -m venv .
+.bin/pip install -r requirements.txt
+.bin/pip install -e .
 ./bin/pip install zc.buildout==2.11.3
 ./bin/buildout
-pip install -r requirements.txt
-pip install -e .
 ./bin/python generate_keys.py
 ./bin/pip install mod_wsgi
