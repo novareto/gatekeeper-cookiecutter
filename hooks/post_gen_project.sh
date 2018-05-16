487a8fb798d56a8bbed2716ee65651c37eac40e7
Souheil CHELFOUH <trollfot@gmail.com>
2018-05-16 15:13:55 +0200

    Using pip instead of buildout for git checkouts

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,5 +3,7 @@
 python3.6 -m venv .
 ./bin/pip install zc.buildout==2.11.3
 ./bin/buildout
+pip install -r requirements.txt
+pip install -e .
 ./bin/python generate_keys.py
 ./bin/pip install mod_wsgi
--- {{cookiecutter.directory_name}}/app.py
+++ {{cookiecutter.directory_name}}/app.py
@@ -12,20 +12,14 @@ class Configuration(object):
         if not os.path.isfile(filename):
             raise RuntimeError('Configuration file does not exist.')
         self.environ = filename
-        self.backup = sys.path[:]
 
     def __enter__(self):
         with open(self.environ, "r") as fd:
             env = json.load(fd)
-
-        paths = env['paths']
-        sys.path[0:0] = paths
         return env['conf']
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        # we need to make something about error handling.
-        sys.path = self.backup
-
+        pass
 
 
 with Configuration('config.json') as config:
