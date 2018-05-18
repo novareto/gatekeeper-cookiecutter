8e0d60808429877f64b71628d254bdecc1039a8c
Souheil CHELFOUH <trollfot@gmail.com>
2018-05-18 11:48:23 +0200

    Added missing file

---
+++ deploy.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import os
+from cookiecutter.main import cookiecutter
+
+
+def main():
+    project_path = os.path.abspath(os.path.dirname(__file__))
+    extra_context={'fullpath': os.path.abspath('.')}
+    cookiecutter(project_path, extra_context=extra_context)
+
+    
+if __name__ == '__main__':
+    main()
