2caeced04534f24a6f1d159b727846afa59f4b3c
Souheil CHELFOUH <trollfot@gmail.com>
2018-05-23 16:46:59 +0200

    Using base layout

--- src/gatekeeper_cookiecutter/template/{{cookiecutter.directory_name}}/keeper/layout.py
+++ src/gatekeeper_cookiecutter/template/{{cookiecutter.directory_name}}/keeper/layout.py
@@ -4,13 +4,10 @@ import crom
 from zope.interface import Interface
 from gatekeeper.layout import Layout
 from cromlech.browser import IRequest, ILayout
-from .resources import gkcss
-from . import tal_template
 
 
 @crom.component
 @crom.sources(IRequest, Interface)
 @crom.target(ILayout)
 class GKLayout(Layout):
-    resources = [gkcss]
-    template = tal_template('layout.pt')
+    pass
