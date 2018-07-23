ffe59ba2e044b06113370b9ec554c52f44047a2d
Souheil CHELFOUH <trollfot@gmail.com>
2018-07-23 13:25:19 +0200

    Activating and using local pip then

--- src/gatekeeper_cookiecutter/template/hooks/post_gen_project.sh
+++ src/gatekeeper_cookiecutter/template/hooks/post_gen_project.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
 python3.6 -m venv .
-./bin/pip install --upgrade pip
-./bin/pip install -r requirements.txt
-./bin/pip install -e .
-./bin/python generate_keys.py
+source ./bin/activate
+pwd
+pip install --upgrade pip
+pip install -r requirements.txt
+pip install -e .
+python generate_keys.py
